993df333153c00bb7eb57fd526ebd02ccf28cc03
Jesse <jcahill225@gmail.com>
2019-12-03 23:27:30 -0500

    reworked main to facilitate one-liners down the road

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-from pathlib import Path
 import librosa
 
-from feature_extract import temporal
+from feature_extract.feature_extract import extract_feature_vector
+from learn_kde_audio import map_factor_learn, emotion_space_map
+from recommend import recommend_songs
 
 
-def main():
-    test_song = Path.cwd() / 'data' / 'raw' / 'clips_45seconds' / '69.mp3'
-    signal, sr = librosa.load(str(test_song))
+def main(song_path, train_audio, train_pdfs):
+    """Performs all needed actions for the project when a new song is uploaded
 
-    temporal_features = temporal.extract_temporal_features(signal, sr)
+    1. Performs feature extraction and dimensionality reduction on the new song
+    2. Learns mapping factors from the training audio features and new song audio features
+    3. Performs emotion space mapping with training PDFs and mapping factors
+    4. Performs recommendation by comparing new PDF to all existing PDFs based on euclidean distance
+    """
 
-    return temporal_features
+    signal, sr = librosa.load(str(song_path))
+
+    # 1
+    all_features = extract_feature_vector(signal, sr)
+
+    # 2
+    map_factor = map_factor_learn(train_audio, all_features)
+
+    # 3 - TODO
+    song_pdf = emotion_space_map(train_pdfs, map_factor)
+
+    # 4 - TODO
+    recommend = recommend_songs(song_pdf, train_pdfs)
+
+
 
 
 if __name__ == "__main__":
