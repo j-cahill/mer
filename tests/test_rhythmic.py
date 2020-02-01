1ee5aee7d774bd24405632dee90154c081fd883d
Jesse <jcahill225@gmail.com>
2020-02-01 14:23:40 -0500

    added .travis.yml

---
+++ tests/test_rhythmic.py
@@ -0,0 +1,14 @@
+import librosa
+import pathlib
+import pytest
+
+import mer.feature_extract.rhythmic
+
+# @pytest.fixture
+# def song_data():
+#     data_path = Pathlib
+#     return librosa.load(str(path))
+
+
+def test_extract_rhythmic_features():
+    assert 1 == 1
