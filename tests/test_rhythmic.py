2b3195cff83df6280e49adab0165a63d571f48bd
Jesse <jcahill225@gmail.com>
2020-02-04 20:24:56 -0500

    testing updates

--- tests/test_rhythmic.py
+++ tests/test_rhythmic.py
@@ -1,14 +1,22 @@
-import librosa
-import pathlib
 import pytest
 
 import mer.feature_extract.rhythmic
 
 # @pytest.fixture
-# def song_data():
+# def song_data(scope='module'):
+#     import pathlib
+#     import librosa
+#
 #     data_path = Pathlib
 #     return librosa.load(str(path))
 
 
 def test_extract_rhythmic_features():
+    from pathlib import Path
+    print('hi')
+    print(Path(__file__) / 'tests' / 'data')
     assert 1 == 1
+
+if __name__ == "__main__":
+    from pathlib import Path
+    print(Path(__file__).parent)
